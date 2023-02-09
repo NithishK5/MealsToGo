008e88261f51d83a9131c86706c1b2a8a2d2ea7b
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2023-02-09 12:40:27 +0000

    activity indicator loaded

--- src/features/screens/restaurantScreens.js
+++ src/features/screens/restaurantScreens.js
@@ -6,6 +6,7 @@ import { Spacer } from "../../components/spacer/spacerComponent";
 import styled from "styled-components";
 import { SafeArea } from "../../components/safeAreaComponent";
 import { RestaurantsContext } from "../../services/restaurants/restaurantContext";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -17,10 +18,25 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
