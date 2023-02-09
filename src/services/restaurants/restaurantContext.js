02fbc3949bb182578d72ac5100ded43db1e4516d
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2023-02-09 14:37:09 +0000

    tidied code

--- src/services/restaurants/restaurantContext.js
+++ src/services/restaurants/restaurantContext.js
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-} from "react";
+import React, { useState, createContext, useContext, useEffect } from "react";
 
 import { restaurantRequest, restaurantTransform } from "./restaurantsService";
 import { LocationContext } from "../location/locationContext";
